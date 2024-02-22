6e5900eb70ebde83075c205cd027f8aae2f93f0c
skiloop <skiloop@gmail.com>
2024-02-22 16:10:37 +0800

    fix: fix version

--- version/version.go
+++ version/version.go
@@ -3,11 +3,11 @@ package version
 import "fmt"
 
 var (
-	Version        = "dev"
-	CommitHash     = "n/a"
-	BuildTimestamp = "n/a"
+	Version    = "dev"
+	CommitHash = "n/a"
+	BuildTime  = "n/a"
 )
 
 func BuildVersion() string {
-	return fmt.Sprintf("%s-%s (%s)", Version, CommitHash, BuildTimestamp)
+	return fmt.Sprintf("%s-%s (%s)", Version, CommitHash, BuildTime)
 }
