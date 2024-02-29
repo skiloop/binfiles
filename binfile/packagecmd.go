4c697a077c4399ba1ff27498bd2237d56f1a9884
skiloop <skiloop@gmail.com>
2024-02-29 10:45:37 +0800

    fix: fix repack cmd helper

--- binfile/packagecmd.go
+++ binfile/packagecmd.go
@@ -15,7 +15,7 @@ type RepackCmd struct {
 	Source              string `arg:"" help:"source bin file name"`
 	Target              string `arg:"" help:"target bin file name"`
 	Workers             int    `short:"w" help:"number of workers" default:"3"`
-	Merge               bool   `short:"m" help:"merge result into one when split > 0" default:"true"`
+	Merge               bool   `short:"m" help:"merge result into one when split > 0"`
 	Split               int    `help:"split target into small parts if positive every specified number of docs, 0 means not to split" default:"0"`
 	TargetCompressType  string `short:"t" help:"compression type for docs in target file, none for no compression" enum:"gzip,bz2,none" default:"none"`
 	PackageCompressType string `short:"c" help:"compression type after whole target completed, none for no compression" enum:"gzip,bz2,none" default:"none"`
